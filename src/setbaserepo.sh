877138dfac4b1085908eed5525988ba73222adbe
Andre Fernandes <andre@vertigo.com.br>
2016-01-20 05:19:43 -0200

    fixes

--- src/setbaserepo.sh
+++ src/setbaserepo.sh
@@ -8,8 +8,11 @@
 # uncomments the "baseurl" entry and replaces
 # "mirror.centos.org" by "registry.vtg/repos"
 #
-echo "changing yum repos..."
-sed "s/^enabled.*/enabled=0/" -i /etc/yum/pluginconf.d/fastestmirror.conf 
-sed "s/^mirrorlist/#mirrorlist/g" -i /etc/yum.repos.d/CentOS-Base.repo
-sed "s/^#baseurl/baseurl/g" -i /etc/yum.repos.d/CentOS-Base.repo
-sed "s/mirror\.centos\.org/registry\.vtg\/repos/g" -i /etc/yum.repos.d/CentOS-Base.repo
+CODE=$(curl -I http://registry.vtg/repos/epel/7/x86_64/repodata/repomd.xml 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+if [ "$CODE" == "200" ]; then
+    echo "changing yum repos..."
+    sed "s/^enabled.*/enabled=0/" -i /etc/yum/pluginconf.d/fastestmirror.conf
+    sed "s/^mirrorlist/#mirrorlist/g" -i /etc/yum.repos.d/CentOS-Base.repo
+    sed "s/^#baseurl/baseurl/g" -i /etc/yum.repos.d/CentOS-Base.repo
+    sed "s/mirror\.centos\.org/registry\.vtg\/repos/g" -i /etc/yum.repos.d/CentOS-Base.repo
+fi
--- src/setepelrepo.sh
+++ src/setepelrepo.sh
@@ -10,8 +10,12 @@
 # this script assumes that the "epel" repo is the first
 # entry at "/etc/yum.repos.d/epel.repo"
 #
-echo "changing yum epel repo..."
-sed "s/^mirrorlist/#mirrorlist/g" -i /etc/yum.repos.d/epel.repo
-# using sed to replace first ocurrence only
-sed "0,/^#baseurl/s//baseurl/" -i /etc/yum.repos.d/epel.repo
-sed "0,/download\.fedoraproject\.org\/pub\/epel\/7\/$basearch/s//registry\.vtg\/repos\/epel\/7\//" -i /etc/yum.repos.d/epel.repo
+# detects at "docker build" time if local repo is available
+CODE=$(curl -I http://registry.vtg/repos/epel/7/x86_64/repodata/repomd.xml 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+if [ "$CODE" == "200" ]; then
+    echo "changing yum epel repo..."
+    sed "s/^mirrorlist/#mirrorlist/g" -i /etc/yum.repos.d/epel.repo
+    # using sed to replace first ocurrence only
+    sed "0,/^#baseurl/s//baseurl/" -i /etc/yum.repos.d/epel.repo
+    sed "0,/download\.fedoraproject\.org\/pub\/epel\/7\/$basearch/s//registry\.vtg\/repos\/epel\/7\//" -i /etc/yum.repos.d/epel.repo
+fi
