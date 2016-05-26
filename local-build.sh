4c8fdfa8638a3f951acadeba234d94893714d9c5
Andre Fernandes <andre@vertigo.com.br>
2016-05-25 23:07:08 -0300

    gitlabci

--- local-build.sh
+++ local-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-BASEREPO="192.168.1.9/repos"
-EPELREPO="http://192.168.1.9/repos/epel/7/x86_64"
+BASEREPO="192.168.1.4/repos"
+EPELREPO="http://192.168.1.4/repos/epel/7/x86_64"
 docker build \
     --build-arg="BASEREPO=$BASEREPO" \
     --build-arg="EPELREPO=$EPELREPO" \
